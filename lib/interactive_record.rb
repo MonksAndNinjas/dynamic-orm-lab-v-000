89e8adb1bacc423c25ab2cdda01b4fea6ecd7403
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-13 17:13:02 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute:)
-    student = DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", attribute)
+    student = DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE attribute = ?", attribute)
   end
 
 end
